9e453e30653697aecfdae8103be6a8d803b43964
Ralf Becker <RalfBecker@outdoor-training.de>
2021-12-04 10:14:26 +0200

    declare views and tables (with skip_create_tables) to allow a recent EGroupware to deinstall Guacamole

--- setup/setup.inc.php
+++ setup/setup.inc.php
@@ -16,7 +16,34 @@ $setup_info['guacamole']['name']      = 'guacamole';
 $setup_info['guacamole']['version']   = '0.2';
 $setup_info['guacamole']['app_order'] = 5;
 $setup_info['guacamole']['enable']    = 1;
-$setup_info['guacamole']['tables']    = [];
+$setup_info['guacamole']['tables']    = array(
+	'guacamole_connection_group',
+	'guacamole_connection',
+	'guacamole_sharing_profile',
+	'guacamole_connection_parameter',
+	'guacamole_sharing_profile_parameter',
+	'guacamole_user_attribute',
+	'guacamole_user_group_attribute',
+	'guacamole_connection_attribute',
+	'guacamole_connection_group_attribute',
+	'guacamole_sharing_profile_attribute',
+	'guacamole_connection_permission',
+	'guacamole_connection_group_permission',
+	'guacamole_sharing_profile_permission',
+	'guacamole_connection_history',
+	'guacamole_user_history',
+	'guacamole_user_password_history',
+);
+$setup_info['guacamole']['views']    = array(
+	'guacamole_entity',
+	'guacamole_user',
+	'guacamole_user_group',
+	'guacamole_user_group_member',
+	'guacamole_system_permission',
+	'guacamole_user_group_permission',
+	'guacamole_user_permission',
+);
+$setup_info['guacamole']['skip_create_tables'] = true;  // done in default_records to add constrains too
 $setup_info['guacamole']['index']     = 'guacamole.'.Guacamole\Ui::class.'.index&ajax=true';
 
 $setup_info['guacamole']['author'] =
@@ -35,5 +62,5 @@ $setup_info['guacamole']['hooks']['admin'] = Guacamole\Hooks::class.'::menu';
 /* Dependencies for this app to work */
 $setup_info['guacamole']['depends'][] = array(
 	 'appname' => 'api',
-	 'versions' => Array('19.1')
+	 'versions' => Array('21.1')
 );
